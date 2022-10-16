5e327cb444e78f1810a1b53b87c8a06bd4dd8215
zdut <zdut@arv.co.th>
2022-10-16 20:34:58 +0700

    add test job

--- jobs/applicationRepo.groovy
+++ jobs/applicationRepo.groovy
@@ -1,9 +1,20 @@
+def services = ["test-1", "test-2", "test-3"]
+
+def createFolder(path, service) {
+    folder(path + "/" + service) {
+        displayName(service)
+        description('Jobs that are automatically generated for ' + service)
+    }
+}
+
 def generatedFolder = 'generated'
 folder(generatedFolder) {
     displayName('Generated Jobs')
     description('Jobs that are automatically generated.')
 }
 
-repos.each { repo ->
-    println "Creating application repo for: " + repo
+services.each { service ->
+    println "Creating application repo for: " + service
+
+    createFolder(generatedFolder, service)
 }
